6ac8885e2a161f4048aad62c29f588c1f407ab4f
hdl <115802556@qq.com>
2019-04-17 07:58:38 +0800

    ·············
    1111111

--- pages/home/index.js
+++ pages/home/index.js
@@ -14,8 +14,6 @@ const pageConfig = {
   data: {
     banner:[{
       imageUrl:'//static.rong360.com/upload/jpg/83/e6/83e6bfdf4fdd3c6a54fa44c8654a4014.jpg'
-    }, {
-        imageUrl: '//static.rong360.com/upload/jpg/a9/09/a9094c4572195e8eefc8dae928630ef7.jpg'
     },{
         imageUrl: '//static.rong360.com/upload/png/b7/2a/b72a5142d1592212e93b4378599a9590.png'
     }]
@@ -41,9 +39,9 @@ const pageConfig = {
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    // getCurrentAddress((res)=>{
-    //   console.dir(res)
-    // })
+    getCurrentAddress((res)=>{
+      console.dir(res)
+    })
   },
 
   /**
